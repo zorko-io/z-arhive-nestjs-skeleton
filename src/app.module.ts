545340e37ed649e59365e209e5db19719ca3c10a
igornester <inesterenko@nucleushealth.io>
2019-05-11 13:25:30 +0300

    - continue experiments with controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { AuthenticationService } from './authentication/authentication.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController],
-  providers: [AppService],
+  providers: [AppService, AuthenticationService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Get()
+  findAll(@Req() request: Request): string {
+    return 'This action returns zzzz cats';
+  }
+
   @Post()
+  @HttpCode(204)
   create(): string {
     return 'This action adds a new cat';
   }
 
-  @Get()
-  findAll(@Req() request: Request): string {
-    return 'This action returns zzzz cats';
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `This action returns #${id} cat`;
   }
 }
